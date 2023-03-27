8bd499c9d3d17565381621b0414ae72dc8c9b2c9
Polinasss <polina129889@gmail.com>
2023-03-27 21:05:36 +0300

    feat: add warning

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './styles/styles';
 import App from './App';
 
 const app = new App(document.getElementById('shelter'));
+
+
+alert('Господа проверяющие, оооочень прошу прощения, проверьте работу 30-ого числа!!! Я усложнила себе жизнь, сделав SPA. К 30-ому всё будет готово :(');
